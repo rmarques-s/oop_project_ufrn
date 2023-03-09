179ff944387a909745caca09ac5f5fdcae9bd1ca
rafaela.marques <marquesrafaela15@yahoo.com.br>
2023-03-09 17:55:37 -0300

    initial commit

---
+++ backend/app.py
@@ -0,0 +1,4 @@
+from bs4 import BeautifulSoup
+
+#Obtendo o HTML
+url = "https://www.ibge.gov.br/estatisticas/economicas/precos-e-custos/9256-indice-nacional-de-precos-ao-consumidor-amplo.html?edicao=33427"
