f2512f1dacb621cf16446021e3ff9488f44d1a6a
Rafaela Marques <65922146+rmarques-s@users.noreply.github.com>
2023-05-11 08:15:08 -0300

    adicionando retorno de arquivo json

--- project/scraping.py
+++ project/scraping.py
@@ -2,8 +2,15 @@ import urllib3
 import json
 import matplotlib.pyplot
 import numpy
+import csv
 
 http = urllib3.PoolManager()
 urlApiIBGE = "http://servicodados.ibge.gov.br/api/v2/censos/nomes/"
 response = http.request('GET', urlApiIBGE)
 print("Response status: ", response.status)
+
+data_response = response.data.decode('utf-8')
+data_response
+
+data_json = json.loads(data_response)
+data_json
