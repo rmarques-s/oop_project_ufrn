eea0d6437be21e954bf2d78f824a2f30a39bc757
Rafaela <marquesrafaela15@yahoo.com.br>
2023-05-16 19:37:55 -0300

    scraping names

--- project/scraping.py
+++ project/scraping.py
@@ -1,9 +1,29 @@
-import urllib3
-import json
-import matplotlib.pyplot
-import numpy
+import requests
+import csv
 
-http = urllib3.PoolManager()
-urlApiIBGE = "http://servicodados.ibge.gov.br/api/v2/censos/nomes/"
-response = http.request('GET', urlApiIBGE)
-print("Response status: ", response.status)
+# Make a GET request to the API
+response = requests.get("http://servicodados.ibge.gov.br/api/v2/censos/nomes/")
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    data = response.json()
+
+    # Specify the CSV file path
+    csv_file_path = "data.csv"
+
+    # Extract relevant data from the API response
+    extracted_data = []
+    for entry in data:
+        name = entry.get("nome", "")
+        region = entry.get("localidades", [{}])[0].get("nome", "")
+        extracted_data.append([name, region])
+
+    # Write the data to a CSV file
+    with open(csv_file_path, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["Name", "Region"])
+        writer.writerows(extracted_data)
+
+    print("Data successfully stored in", csv_file_path)
+else:
+    print("Failed to retrieve data from the API.")
